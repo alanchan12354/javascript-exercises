404252ed2fd8ad96f0ea09d0be6e24b4cc599eb3
CheungHong CHAN <67678585+alanchan12354@users.noreply.github.com>
2025-09-08 17:01:30 +0800

    Done ex7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(x) {
+    let y = "";
+    for (let i = 0; i < x.length; i++) {
+        y += x[x.length - 1 - i];
+    }
+    return y;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
