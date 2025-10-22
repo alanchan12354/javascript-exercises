d816b824f83e8e43312b35e28ea42aba4019ab76
CheungHong CHAN <67678585+alanchan12354@users.noreply.github.com>
2025-10-22 11:43:32 +0800

    fix: update function signatures for repeatString and removeFromArray to include parameters

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rmTarget) {
+    tempArr = [...arr];
+    for (let i = 0; i < tempArr.length; i++) {
+        if (tempArr[i] === rmTarget) {
+            tempArr.splice(i, 1)
+            i--;
+        }
+    }
+    return tempArr;
 };
 
 // Do not edit below this line
