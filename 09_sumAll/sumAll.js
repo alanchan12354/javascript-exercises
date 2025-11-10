fed286d7594f5822be9bbc3521cc8dbcec13fa0b
CheungHong CHAN <67678585+alanchan12354@users.noreply.github.com>
2025-11-10 20:15:03 +0800

    fix: update sumAll and leapYears functions to include parameter checks and return errors for invalid inputs; enable all tests in respective spec files

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    // Integer check
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    // Negative check
+    if (a < 0 || b < 0) return 'ERROR';
+    let small, big;
+    if (a > b) {
+        small = b;
+        big = a;
+    } else {
+        small = a;
+        big = b;
+    }
+    let sum = 0;
+    for (let i = small; i < big + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,36 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if (typeof f === 'number' && !isNaN(f)) {
+    let temp = (f - 32) * 5 / 9;
+    temp = Number(temp.toFixed(1));
+    return temp;
+  }
+  return "ERROR";
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if (typeof c === 'number' && !isNaN(c)) {
+    let temp = c * 9 / 5 + 32;
+    temp = Number(temp.toFixed(1));
+    return temp;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
+};
+
+
+
+
+const convertToCelsius = function(f) {
+  if (!(typeof f === 'number' && !isNaN(f))) {
+    return "ERROR";
+  }
+  let temp = (f - 32) * 5 / 9;
+  temp = Number(temp.toFixed(1));
+  return temp;
+  
 };
